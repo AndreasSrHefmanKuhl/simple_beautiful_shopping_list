5a73b12847d1899c0f5768a6ca93cd4bb0950efe
AndreasKuhl <hefbolzen@gmail.com>
2024-09-25 09:47:04 +0200

    fin

--- lib/presentation/list_content/separated_list_view_list_content.dart
+++ lib/presentation/list_content/separated_list_view_list_content.dart
@@ -2,23 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:simple_beautiful_shopping_list/models/product.dart';
 import 'package:simple_beautiful_shopping_list/presentation/list_entry.dart';
 
-class SeparatedListViewListContent extends StatelessWidget {
-  const SeparatedListViewListContent({super.key, required this.products});
+class ColumnListContent extends StatelessWidget {
+  const ColumnListContent({super.key, required this.products});
 
   final List<Product> products;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListEntry(product: products[index]),
-              const SizedBox(height: 16),
-            ],
-          );
-        });
+    return ListView.separated(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ListEntry(product: products[index]);
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(height: 16); // Adjust the height as needed
+      },
+    );
   }
-}// Hier soll eine ListView zurückgegeben werden.
+}
